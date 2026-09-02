902e5bd1b2e4778cce29aacab55c051ba187a395
Ruhit Paul <ruhitpaul18@gmail.com>
2026-09-02 11:08:44 +0600

    Add getChaseVerdict function to evaluate match outcomes

--- answer.js
+++ answer.js
@@ -56,3 +56,24 @@ function getCngFare(distance, isNight = false, waitingMinutes = 0) {
 }
 
 
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    const runsNeeded = target - scored;
+    if (runsNeeded <= 0) {
+        return "Won"
+    } else if (ballsLeft <= 0 && runsNeeded >= 0) {
+        return "Lost"
+    }
+
+    const requiredRate = (runsNeeded / ballsLeft) * 6
+    let wonPossibility = null
+
+    if (requiredRate <= 6) {
+        wonPossibility = "Comfortable"
+    } else if(requiredRate > 6 && requiredRate <= 12) {
+        wonPossibility = "Tough"
+    } else {
+        wonPossibility = "Almost Impossible"
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${wonPossibility}`
+}
